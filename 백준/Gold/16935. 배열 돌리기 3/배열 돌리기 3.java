94406c276a9d53bd96a13aed206792d63fe25a80
Aeraekun <68500724+Aeraekun@users.noreply.github.com>
2024-02-07 17:20:55 +0900

    [Gold V] Title: 배열 돌리기 3, Time: 408 ms, Memory: 70572 KB -BaekjoonHub

---
+++ 백준/Gold/16935. 배열 돌리기 3/배열 돌리기 3.java
@@ -0,0 +1,104 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int r = Integer.parseInt(st.nextToken());
+		int[][] map = new int[n][m];
+		int[] cal = new int[r];
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < m; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < r; i++) {
+			cal[i] = Integer.parseInt(st.nextToken());
+		}
+		for (int c : cal) {
+			n = map.length;
+			m = map[0].length;
+			int iHalf, jHalf;
+			int[][] temp;
+			switch (c) {
+			case 1:
+				temp = new int[n][m];
+				for (int i = 0; i < n; i++) {
+					for (int j = 0; j < m; j++) {
+						temp[n - i - 1][j] = map[i][j];
+					}
+				}
+				map = temp;
+				break;
+			case 2:
+				temp = new int[n][m];
+				for (int i = 0; i < n; i++) {
+					for (int j = 0; j < m; j++) {
+						temp[i][m - j - 1] = map[i][j];
+					}
+				}
+				map = temp;
+				break;
+			case 3:
+				temp = new int[m][n];
+				for (int i = 0; i < n; i++) {
+					for (int j = 0; j < m; j++) {
+						temp[j][n - i - 1] = map[i][j];
+					}
+				}
+				map = temp;
+				break;
+			case 4:
+				temp = new int[m][n];
+				for (int i = 0; i < n; i++) {
+					for (int j = 0; j < m; j++) {
+						temp[m - j - 1][i] = map[i][j];
+					}
+				}
+				map = temp;
+				break;
+			case 5:
+				temp = new int[n][m];
+				iHalf = n / 2;
+				jHalf = m / 2;
+				for (int i = 0; i < iHalf; i++) {
+					for (int j = 0; j < jHalf; j++) {
+						temp[i][j + jHalf] = map[i][j];
+						temp[i + iHalf][j + jHalf] = map[i][j + jHalf];
+						temp[i + iHalf][j] = map[i + iHalf][j + jHalf];
+						temp[i][j] = map[i + iHalf][j];
+					}
+				}
+				map = temp;
+				break;
+			case 6:
+				temp = new int[n][m];
+				iHalf = n / 2;
+				jHalf = m / 2;
+				for (int i = 0; i < iHalf; i++) {
+					for (int j = 0; j < jHalf; j++) {
+						temp[i + iHalf][j] = map[i][j];
+						temp[i][j] = map[i][j + jHalf];
+						temp[i][j + jHalf] = map[i + iHalf][j + jHalf];
+						temp[i + iHalf][j + jHalf] = map[i + iHalf][j];
+					}
+				}
+				map = temp;
+				break;
+			}
+		}
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[0].length; j++) {
+				sb.append(map[i][j]).append(" ");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+}
