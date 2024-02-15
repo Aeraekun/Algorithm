8c8e7bdaec7cc9f86b57936280a1507842dd5bcc
Aeraekun <68500724+Aeraekun@users.noreply.github.com>
2024-02-15 17:51:17 +0900

    [Gold IV] Title: 알파벳, Time: 1156 ms, Memory: 16508 KB -BaekjoonHub

---
+++ 백준/Gold IV/1987. 알파벳/알파벳.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int r, c, ans = 1;
+	static int[] dx = { 1, 0, -1, 0 };
+	static int[] dy = { 0, 1, 0, -1 };
+	static boolean[] alphabet;
+	static boolean[][] visited;
+	static char[][] arr;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		r = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+		alphabet = new boolean[26];
+		visited = new boolean[r][c];
+		arr = new char[r][c];
+		for (int i = 0; i < r; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < c; j++) {
+				arr[i][j] = str.charAt(j);
+			}
+		}
+		dfs(0, 0, 1);
+		System.out.println(ans);
+	}
+
+	private static void dfs(int x, int y, int len) {
+		visited[x][y] = true;
+		alphabet[arr[x][y] - 'A'] = true;
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (nx < 0 || nx >= r || ny < 0 || ny >= c)
+				continue;
+			if (visited[nx][ny] || alphabet[arr[nx][ny] - 'A'])
+				continue;
+			ans = Math.max(ans, len + 1);
+			dfs(nx, ny, len + 1);
+		}
+		visited[x][y] = false;
+		alphabet[arr[x][y] - 'A'] = false;
+	}
+}
