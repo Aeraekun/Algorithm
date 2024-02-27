2955f46fd4048b717fbe533fbf1cd04346813a17
Aeraekun <68500724+Aeraekun@users.noreply.github.com>
2024-02-27 16:29:21 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 128 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -2,39 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int N, ans = 0;
+	static int N;
 	static boolean[][] map;
-	static int[][] pipe = { { 1, 0 }, { 1, 1 }, { 0, 1 } };
-	static int[][] move = { { 0, 1 }, { 0, 1, 2 }, { 1, 2 } };
+	static int[][][] dp;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		N = Integer.parseInt(br.readLine());
 		map = new boolean[N][N];
+		dp = new int[N][N][3];
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
 				map[i][j] = st.nextToken().equals("0") ? true : false;
 			}
 		}
-		Queue<int[]> queue = new ArrayDeque<>();
-		queue.offer(new int[] { 0, 1, 2 });
-		while (!queue.isEmpty()) {
-			int[] now = queue.poll();
-			for (int m : move[now[2]]) {
-				int ny = now[0] + pipe[m][0];
-				int nx = now[1] + pipe[m][1];
-				if (ny < N && nx < N && map[ny][nx]) {
-					if ((m == 1 && map[now[0] + 1][now[1]] && map[now[0]][now[1] + 1]) || m != 1) {
-						if (ny == N - 1 && nx == N - 1)
-							ans++;
-						else
-							queue.offer(new int[] { ny, nx, m });
-					}
+		dp[0][1][0] = 1;
+		for (int y = 0; y < N; y++) {
+			for (int x = 2; x < N; x++) {
+				if (!map[y][x])
+					continue;
+				if (x - 1 >= 0)
+					dp[y][x][0] += dp[y][x - 1][0] + dp[y][x - 1][1];
+				if (y - 1 >= 0)
+					dp[y][x][2] += dp[y - 1][x][2] + dp[y - 1][x][1];
+				if (x - 1 >= 0 && y - 1 >= 0 && map[y][x - 1] && map[y - 1][x]) {
+					for (int i = 0; i < 3; i++)
+						dp[y][x][1] += dp[y - 1][x - 1][i];
 				}
 			}
 		}
-		System.out.println(ans);
+		System.out.println(dp[N - 1][N - 1][0] + dp[N - 1][N - 1][1] + dp[N - 1][N - 1][2]);
 	}
 }
