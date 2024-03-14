d2cfa9f9bb8a8ca38a30493a4d71e9cc2cce3725
Aeraekun <68500724+Aeraekun@users.noreply.github.com>
2024-03-14 23:23:22 +0900

    [Gold V] Title: 치킨 배달, Time: 204 ms, Memory: 15540 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -2,59 +2,74 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int N, M, ans;
+    static List<xy> chi, home;
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
+    static int[][] map;
+    static boolean[] isSelected;
+    static boolean[][] visited;
 
-	static int N, M, ans = Integer.MAX_VALUE;
-	static ArrayList<int[]> hList = new ArrayList<>();
-	static ArrayList<int[]> cList = new ArrayList<>();
-	static int[] selected;
+    static class xy {
+        int y;
+        int x;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+        xy(int y, int x) {
+            this.y = y;
+            this.x = x;
+        }
+    }
 
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		selected = new int[M];
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        ans = Integer.MAX_VALUE;
+        map = new int[N][N];
+        chi = new ArrayList<>();
+        home = new ArrayList<>();
 
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
-				int v = Integer.parseInt(st.nextToken());
-				if (v == 1) {
-					hList.add(new int[] { i, j });
-				} else if (v == 2) {
-					cList.add(new int[] { i, j });
-				}
-			}
-		}
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if (map[i][j] == 1)
+                    home.add(new xy(i, j));
+                else if (map[i][j] == 2)
+                    chi.add(new xy(i, j));
+            }
+        }
+        isSelected = new boolean[chi.size()];
+        comb(0, 0);
+        System.out.println(ans);
+    }
 
-		comb(0, 0);
-		System.out.println(ans);
-	}
+    private static void comb(int start, int now) {
+        if (now == M) {
+            bfs();
+            return;
+        }
+        for (int i = start; i < chi.size(); i++) {
+            if (!isSelected[i]) {
+                isSelected[i] = true;
+                comb(i + 1, now + 1);
+                isSelected[i] = false;
+            }
+        }
+    }
 
-	static int dist(int[] home, int[] chi) {
-		return Math.abs(home[0] - chi[0]) + Math.abs(home[1] - chi[1]);
-	}
-
-	static void comb(int cnt, int start) {
-		if (cnt == M) {
-			int sum = 0;
-			for (int i = 0; i < hList.size(); i++) {
-				int minDist = Integer.MAX_VALUE;
-				for (int j = 0; j < M; j++) {
-					minDist = Math.min(minDist, dist(hList.get(i), cList.get(selected[j])));
-				}
-				sum += minDist;
-			}
-			ans = Math.min(ans, sum);
-			return;
-		}
-
-		for (int i = start; i < cList.size(); i++) {
-			selected[cnt] = i;
-			comb(cnt + 1, i + 1);
-		}
-	}
-
+    private static void bfs() {
+        int temp = 0;
+        for (int h = 0; h < home.size(); h++) {
+            int mt = Integer.MAX_VALUE;
+            for (int c = 0; c < chi.size(); c++) {
+                if (isSelected[c]) {
+                    mt = Math.min(mt, Math.abs(chi.get(c).y - home.get(h).y) + Math.abs(chi.get(c).x - home.get(h).x));
+                }
+            }
+            temp += mt;
+        }
+        ans = Math.min(ans, temp);
+    }
 }
