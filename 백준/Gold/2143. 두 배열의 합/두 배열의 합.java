0ac2c93d71af0605586dc93576795de250c3bd3d
Aeraekun <68500724+Aeraekun@users.noreply.github.com>
2024-06-20 17:58:30 +0900

    [Gold III] Title: 두 배열의 합, Time: 1764 ms, Memory: 100848 KB -BaekjoonHub

---
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int T, l1, l2;
+    static long ans;
+    static Map<Integer, Integer> map1, map2;
+    static int[] arr1, arr2, temp1, temp2;
+    static Integer[] set1, set2;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        ans = 0;
+        T = Integer.parseInt(br.readLine());
+        l1 = Integer.parseInt(br.readLine());
+        map1 = new HashMap<>();
+        arr1 = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        temp1 = new int[arr1.length];
+        l2 = Integer.parseInt(br.readLine());
+        map2 = new HashMap<>();
+        arr2 = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        temp2 = new int[arr2.length];
+        calc(map1, arr1, temp1);
+        calc(map2, arr2, temp2);
+        set1 = map1.keySet().toArray(new Integer[0]);
+        set2 = map2.keySet().toArray(new Integer[0]);
+        Arrays.sort(set1);
+        Arrays.sort(set2, Collections.reverseOrder());
+        check();
+        System.out.println(ans);
+    }
+
+    private static void calc(Map<Integer, Integer> map, int[] arr, int[] temp) {
+        for (int s = 0; s < arr.length; s++) {
+            for (int i = 0; i < arr.length - s; i++) {
+                temp[i] += arr[i + s];
+                if (map.containsKey(temp[i])) map.put(temp[i], map.get(temp[i]) + 1);
+                else map.put(temp[i], 1);
+            }
+        }
+    }
+
+    private static void check() {
+        int i = 0, j = 0;
+        while (i < set1.length && j < set2.length) {
+            if (set1[i] + set2[j] == T) {
+                ans += (long) map1.get(set1[i]) * map2.get(set2[j]);
+                i++;
+                j++;
+            } else if (set1[i] + set2[j] < T) i++;
+            else j++;
+        }
+    }
+}
