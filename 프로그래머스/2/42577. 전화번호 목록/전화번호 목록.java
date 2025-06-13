6d96b36511df0c6fd6dfa4702c9c3fa00267308e
Aeraekun <68500724+Aeraekun@users.noreply.github.com>
2025-06-14 00:54:33 +0900

    [level 2] Title: 전화번호 목록, Time: 316.68 ms, Memory: 99.5 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,11 +1,11 @@
+import java.io.*;
 import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
         Arrays.sort(phone_book);
-        for(int i=0;i<phone_book.length - 1;i++){
-            if(phone_book[i+1].startsWith(phone_book[i]))
-                return false;
+        for(int i=0;i<phone_book.length-1;i++){
+                if(phone_book[i+1].indexOf(phone_book[i])==0) return false;
         }
         return true;
     }
