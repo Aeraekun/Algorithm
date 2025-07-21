b6e14ed14a4876fd1b497dcac60e9a3dc335e033
Aeraekun <68500724+Aeraekun@users.noreply.github.com>
2025-07-21 21:45:07 +0900

    [Silver IV] Title: 보물, Time: 68 ms, Memory: 11616 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.java
+++ 백준/Silver/1026. 보물/보물.java
@@ -2,17 +2,13 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, ans;
-    static int[] A, B;
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int ans = 0;
+        int[] A = new int[N];
+        int[] B = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        ans = 0;
-        N = Integer.parseInt(st.nextToken());
-        A = new int[N];
-        B = new int[N];
-        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) A[i] = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) B[i] = Integer.parseInt(st.nextToken());
