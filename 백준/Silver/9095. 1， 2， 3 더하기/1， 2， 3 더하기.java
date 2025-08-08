9694b18f1ebae74edd28dbd54959cdc615695acf
Aeraekun <68500724+Aeraekun@users.noreply.github.com>
2025-08-08 17:32:45 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 68 ms, Memory: 11528 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -13,12 +13,13 @@ public class Main {
     }
 
     private static int cal(int n) {
-        int[] dp = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            if (i == 1) dp[1] = 1;
-            else if (i == 2) dp[2] = 2;
-            else if (i == 3) dp[3] = 4;
-            else dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        int[] dp = new int[12];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        if (n < 4) return dp[n];
+        for (int i = 4; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
         }
         return dp[n];
     }
