87ba9364db262f2da818aa43f66f64283200f094
Aeraekun <68500724+Aeraekun@users.noreply.github.com>
2025-08-09 01:12:04 +0900

    [level 3] Title: 입국심사, Time: 57.46 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -4,28 +4,27 @@ import java.util.*;
 class Solution {
 
     public long solution(int n, int[] times) {
-        int max = Arrays.stream(times).max().getAsInt();
-        System.out.println(max);
-        long left = 1, right = (long) max * n;
-        long answer = right;
-
-        while (left <= right) {
+        int max =0;
+        for(int i:times){
+            max = Math.max(max,i);
+        }
+        long left = 0;
+        long right = (long) max * n;
+        long ans = right;
+        while(left<=right) {
             long mid = (left + right) / 2;
-
-            // mid 시간 동안 처리 가능한 총 인원수 계산
-            long count = Arrays.stream(times).mapToLong(i -> mid / i).sum();
-
-            if (count < n) {
-                // 부족하므로 시간을 늘림
-                left = mid + 1;
-            } else {
-                // 충분하므로 시간을 줄이면서 더 짧은 시간 탐색
-                answer = mid;
-                right = mid - 1;
+            long temp = 0;
+            for(int i:times){
+                temp+=(mid/i);
+            }
+            if(temp<n){
+                left=mid+1;
+            }else{
+                ans=mid;
+                right=mid-1;
             }
         }
-
-        return answer;
+        return ans;
     }
 
 }
