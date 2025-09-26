dbb656f0f21647e3c55cdc0034112ff200326402
Aeraekun <68500724+Aeraekun@users.noreply.github.com>
2025-09-27 03:07:40 +0900

    [level 4] Title: 도둑질, Time: 27.37 ms, Memory: 102 MB -BaekjoonHub

---
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] money) {
+        int n = money.length;
+        int[][] dp = new int[2][n];
+        dp[0][0]=money[0];
+        dp[0][1]=money[0];
+        dp[1][1]=money[1];
+        for(int i=2;i<n-1;i++) dp[0][i]=Math.max(dp[0][i-2]+money[i], dp[0][i-1]);
+        for(int i=2;i<n;i++) dp[1][i]=Math.max(dp[1][i-2]+money[i], dp[1][i-1]);
+        return Math.max(dp[0][n-2],dp[1][n-1]);
+    }
+}
