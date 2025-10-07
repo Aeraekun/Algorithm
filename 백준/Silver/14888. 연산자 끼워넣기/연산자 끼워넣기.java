4c794cf3dc33f6e3933f979284b9b8f38d939e7c
Aeraekun <68500724+Aeraekun@users.noreply.github.com>
2025-10-07 17:41:58 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 76 ms, Memory: 13764 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+    static int[] num;
+    static int[] operator = new int[4];
+    static int[] count = new int[4];
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        num = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) num[i] = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 4; i++) operator[i] = Integer.parseInt(st.nextToken());
+        dfs(num[0], 1);
+        System.out.println(max);
+        System.out.println(min);
+    }
+
+    private static void dfs(int now, int order) {
+        if (order == N) {
+            if (now > max) max = now;
+            if (now < min) min = now;
+            return;
+        }
+        for (int i = 0; i < 4; i++) {
+            if (operator[i] != 0) {
+                operator[i]--;
+                if (i == 0) dfs(now + num[order], order + 1);
+                else if (i == 1) dfs(now - num[order], order + 1);
+                else if (i == 2) dfs(now * num[order], order + 1);
+                else dfs(now / num[order], order + 1);
+                operator[i]++;
+            }
+        }
+    }
+}
