5d28f50335f64eaeffd17b93443113d7e9b35689
Aeraekun <68500724+Aeraekun@users.noreply.github.com>
2025-10-29 23:17:40 +0900

    [Gold V] Title: 1로 만들기 2, Time: 160 ms, Memory: 23564 KB -BaekjoonHub

---
+++ 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] dp = new int[n + 1];
+		int[] log = new int[n + 1];
+
+		for (int i = 2; i <= n; i++) {
+			dp[i] = dp[i - 1] + 1;
+			log[i] = i - 1;
+			if (i % 2 == 0 && dp[i] > dp[i / 2] + 1) {
+				dp[i] = dp[i / 2] + 1;
+				log[i] = i / 2;
+			}
+			if (i % 3 == 0 && dp[i] > dp[i / 3] + 1) {
+				dp[i] = dp[i / 3] + 1;
+				log[i] = i / 3;
+			}
+		}
+		System.out.println(dp[n]);
+		while (n > 0) {
+			System.out.print(n + " ");
+			n = log[n];
+		}
+	}
+}
+
