9162d32b40779ed90f5c918aff13bfad210fdc98
Aeraekun <68500724+Aeraekun@users.noreply.github.com>
2026-01-29 23:23:31 +0900

    [Gold III] Title: 줄 세우기, Time: 352 ms, Memory: 45796 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -2,45 +2,44 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int N, M;
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	
-	public static void main(String[] args) throws IOException {
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		int [] in = new int [N+1];
-		ArrayList<Integer>[] list = new ArrayList[N+1];
-		for (int i=1; i<=N; i++) {
-			list[i] = new ArrayList<Integer>();
-		}
-		for (int i=1; i<=M; i++) {
-			String [] input = br.readLine().split(" ");
-			int s = Integer.parseInt(input[0]);
-			int e = Integer.parseInt(input[1]);
-			list[s].add(e);
-			++in[e];
-		}
-		
-		Queue<Integer> q = new ArrayDeque<>();
-		for (int i=1; i<=N; i++) {
-			if (in[i] == 0) {
-				q.offer(i);
-			}
-		}
-		StringBuilder sb = new StringBuilder();
-		while(!q.isEmpty()) {
-			int v = q.poll();
-			sb.append(v).append(" ");
-			for (int i=0, size = list[v].size(); i < size; i++) {
-				int e = list[v].get(i);
-				if (--in[e] == 0) {
-					q.offer(e);
-				}
-			}
-		}
-		System.out.println(sb.toString().trim());
-	}
+    static int N, M;
+    static int[] graph;
+    static List<Integer>[] list;
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        graph = new int[N + 1];
+        list = new ArrayList[N + 1];
+
+        for (int n = 1; n <= N; n++) list[n] = new ArrayList<>();
+        for (int m = 0; m < M; m++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            list[a].add(b);
+            graph[b]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            if (graph[i] == 0) queue.offer(i);
+        }
+
+        while(!queue.isEmpty()) {
+            int now = queue.poll();
+            sb.append(now).append(' ');
+            for(int i:list[now]) {
+                graph[i]--;
+                if(graph[i]==0) queue.offer(i);
+            }
+        }
+
+        System.out.println(sb);
+    }
 }
